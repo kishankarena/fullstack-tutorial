6315cf0d14d5b57e9c941fc23dd7fdaae2734a17
KKsimform <kishan.k@simformsolutions.com>
2022-09-01 15:59:00 +0530

    update data using implement of Mutation

--- start/client/src/index.tsx
+++ start/client/src/index.tsx
@@ -2,6 +2,7 @@ import {
   ApolloClient,
   ApolloProvider,
   NormalizedCacheObject,
+  gql,
 } from '@apollo/client';
 import { cache } from './cache';
 import React from 'react';
@@ -9,9 +10,20 @@ import ReactDOM from 'react-dom';
 import Pages from './pages';
 import injectStyles from './styles';
 
+export const typeDefs = gql`
+  extend type Query {
+    isLoggedIn: Boolean!
+    cartItems: [ID!]!
+  }
+`;
+
 const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
   cache,
   uri: 'http://localhost:4000/graphql',
+  headers: {
+    authorization: localStorage.getItem('token') || '',
+  },
+  typeDefs,
 });
 
 injectStyles();
--- start/client/src/pages/login.tsx
+++ start/client/src/pages/login.tsx
@@ -1,5 +1,29 @@
 import React from 'react';
+import { gql, useMutation } from '@apollo/client';
+import { Loading, LoginForm } from '../components';
+import * as LoginTypes from './__generated__/Login';
 
+export const LOGIN_USER = gql`
+  mutation Login($email: String) {
+    login(email: $email) {
+      id
+      token
+    }
+  }
+`;
 export default function Login() {
-  return <div />;
+  const [login, { loading, error }] = useMutation<
+    LoginTypes.Login,
+    LoginTypes.LoginVariables
+  >(LOGIN_USER, {
+    onCompleted: ({ login }) => {
+      if (login) {
+        localStorage.setItem('token', login.token as string);
+        localStorage.setItem('userId', login.id as string);
+      }
+    },
+  });
+  if (loading) return <Loading />;
+  if (error) return <p>An error occured</p>;
+  return <LoginForm login={login} />;
 }
